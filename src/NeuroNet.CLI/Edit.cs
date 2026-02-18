5d48ae1fa9d94fd0aca18675b6aaa9ad586608d9
Lou <max@aicheler1.de>
2026-02-18 12:44:57 +0100

    Added Edit function
    Implement Edit Network functionality in CLI
    Most options don't work yet

--- src/NeuroNet.CLI/Edit.cs
+++ src/NeuroNet.CLI/Edit.cs
@@ -14,4 +14,71 @@ class EditCLI
         }
         return network;
     }
+
+    public static MultipleValues<List<List<Neuron>>> Edit_Network(List<List<Neuron>> network, string currentnnName)
+    {
+        bool Error = false;
+        do {
+            Console.WriteLine("Edit Neural Network");
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. Randomize Weights");
+            Console.WriteLine("2. Edit Weights Manually");
+            Console.WriteLine("3. Edit Number of Layers & Neurons");
+            Console.WriteLine("4. Edit Name of the Network");
+            //Console.WriteLine("4. Edit Neuron Types");
+            string UserOutputString = Console.ReadLine() ?? string.Empty;
+            if (!int.TryParse(UserOutputString, out int UserOutput))
+            {
+                if (Extras.IsReturn(UserOutputString))
+                {
+                    Console.WriteLine("Returning to Main Menu...");
+                    return new MultipleValues<List<List<Neuron>>>
+                    {
+                        HasError = true,
+                        Value = network,
+                        ErrorMessage = Program.returnString,
+                    };
+                }
+                Console.WriteLine("Please insert a valid number");
+                Error = true;
+            }
+            else
+            {
+                switch (UserOutput)
+                {
+                    case 1:
+                        network = Edit.RandomizeWeights(network);
+                        SaveCLI.SaveNetworkToFileY(network, "overwrite", currentnnName);
+                        Console.WriteLine("Weights randomized and saved to file");
+                        break;
+                    case 2:
+                        /*TwoValues<List<List<Neuron>>, bool> EditWeightsResult = Edit.EditWeightsManually(network);
+                        network = EditWeightsResult.Value1;
+                        Error = EditWeightsResult.Value2;
+                        if (!Error)
+                        {
+                            SaveCLI.SaveNetworkToFileY(network, "overwrite", currentnnName);
+                            Console.WriteLine("Weights edited and saved to file");
+                        } */
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Please insert one of the shown Options");
+                        Error = true;
+                        break;
+                }
+            }
+        } while (Error);
+
+
+
+        return new MultipleValues<List<List<Neuron>>>
+        {
+          Value = network,
+          HasError = false,
+        };
+    }
 }
--- src/NeuroNet.CLI/Program.cs
+++ src/NeuroNet.CLI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.NetworkInformation;
+﻿using System.Diagnostics;
+using System.Net.NetworkInformation;
 using NeuroNet.Core;
 namespace NeuroNet.CLI;
 
@@ -7,183 +8,208 @@ internal class Program
     public const string returnString = "return";
     public static void Main(string[] args)
     {
-        bool again = false;
-        string? Way = null;
-        TwoValues<List<List<Neuron>>, string?> CreationResult = default!;
-        string currentnnName = "MyNeuralNetwork";
-        do {
-            int UserOutput;
-            bool Error;
-            List<List<Neuron>>? LoadedNetwork = null;
-            do
-            {
-                Error = false;
-                if (!again) {
-                    Console.Clear();
-                    Console.WriteLine("NeuroNet");
-                    Console.WriteLine("What would you like to do?");
-                    Console.WriteLine("1. Create a NeuralNetwork");
-                    Console.WriteLine("2. Load a NeuralNetwork");
-                    Console.WriteLine("3. Exit");
+        while (true) {
+            bool again = false;
+            string? Way = null;
+            TwoValues<List<List<Neuron>>, string?> CreationResult = default!;
+            string currentnnName = "MyNeuralNetwork";
+            do {
+                int UserOutput;
+                bool Error;
+                List<List<Neuron>>? LoadedNetwork = null;
+                do
+                {
+                    Error = false;
+                    if (!again) {
+                        Console.Clear();
+                        Console.WriteLine("NeuroNet");
+                        Console.WriteLine("What would you like to do?");
+                        Console.WriteLine("1. Create a NeuralNetwork");
+                        Console.WriteLine("2. Load a NeuralNetwork");
+                        Console.WriteLine("3. Exit");
+                        string UserOutputString = Console.ReadLine() ?? string.Empty;
+                        if (!int.TryParse(UserOutputString, out UserOutput))
+                        {
+                            if (Extras.IsReturn(UserOutputString))
+                            {
+                                Console.WriteLine("Exiting Program...");
+                                Error = true;
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please type in a valid number");
+                                Extras.PressKey();
+                                Error = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        switch (Way)
+                        {
+                            case "Create":
+                                UserOutput = 1;
+                                break;
+                            case "Load":
+                                UserOutput = 2;
+                                break;
+                            default:
+                                Console.WriteLine("This should never happen, but if it does, just restart the program & Report on GitHub.");
+                                return;
+                        }
+                    }
+                    switch (UserOutput)
+                    {
+                        case 1:
+                            Way = "Create";
+                            bool repeat;
+                            if (again) repeat = true;
+                            else repeat = false;
+                            do {
+                                CreationResult = CreateCLI.CreatingProcess(CreationResult, again);
+                                repeat = false;
+                                if (CreationResult.Value2 == returnString)
+                                {
+                                    Console.WriteLine("Exiting Neural Network Creation...");
+                                    Error = true;
+                                }
+                                else {
+                                    LoadedNetwork = CreationResult.Value1 ?? throw new Exception("Loaded Network cannot be null");
+                                    if (again)
+                                    {
+                                        Console.WriteLine("Deleting old Network & Creating new Network...");
+                                        if (SaveCLI.DeleteFile(currentnnName)) Extras.PressKey();
+                                        else //Return Keyword was typed in
+                                        {
+                                            Console.WriteLine("Why do you do that?");
+                                            Console.WriteLine("Do you think I have no Life");
+                                            Console.WriteLine("I have feelings too, you know...");
+                                            Console.WriteLine("Please just type in the same name and everything will be fine...");
+                                            Console.WriteLine();
+                                        }
+                                    }
+                                    currentnnName = SaveCLI.SaveNetworkToFile(LoadedNetwork, "new");
+                                    again = false;
+                                    if (currentnnName.ToLower() == returnString.ToLower()) repeat = true;
+                                }
+                            }
+                            while(repeat);
+                            break;
+                        case 2:
+                            Way = "Load";
+                            var result = LoadCLI.LoadNeuralNetwork();
+                            if (result.HasError)
+                            {
+                                Error = true;
+                                if (result.ErrorMessage != "User exited load process.") Extras.PressKey();
+                            }
+                            else
+                            {
+                                LoadedNetwork = (result.Value ?? throw new Exception("Network and network name cannot be null")).Value1 ?? throw new Exception("Network cannot be null");
+                                currentnnName = result.Value.Value2 ?? throw new Exception("Network name cannot be null");
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("Exiting Program...");
+                            return;
+                        default:
+                            if(!Error) {
+                                Console.WriteLine("Please type in one of the shown options");
+                                Extras.PressKey();
+                                Error = true;
+                            }
+                            break;
+                    }
+                    again = false;
+                } while (Error);
+                if (LoadedNetwork is null) throw new InvalidOperationException("LoadedNetwork must not be null here");
+
+                Extras.PressKey();
+                LoadedNetwork = EditCLI.RandomizeIfNeeded(LoadedNetwork, currentnnName);
+                UserOutput = 0;
+                do {
+                    Error = false;
+                    Console.WriteLine();
+                    Console.WriteLine("What do you want to do?");
+                    Console.WriteLine("1. Run the Neural Network");
+                    Console.WriteLine("2. Let the Neural Network learn");
+                    Console.WriteLine("3. Edit the Neural Network");
+                    Console.WriteLine("4. Return to Load/Create Menu");
+                    Console.WriteLine("5. Exit");
                     string UserOutputString = Console.ReadLine() ?? string.Empty;
                     if (!int.TryParse(UserOutputString, out UserOutput))
                     {
-                        if (Extras.IsReturn(UserOutputString))
-                        {
-                            Console.WriteLine("Exiting Program...");
-                            Error = true;
-                            return;
-                        }
-                        else
-                        {
+                        if (Extras.IsReturn(UserOutputString)) again = true;
+                        else {
                             Console.WriteLine("Please type in a valid number");
-                            Extras.PressKey();
                             Error = true;
                         }
                     }
-                }
-                else
-                {
-                    switch (Way)
+                    else
                     {
-                        case "Create":
-                            UserOutput = 1;
-                            break;
-                        case "Load":
-                            UserOutput = 2;
-                            break;
-                        default:
-                            Console.WriteLine("This should never happen, but if it does, just restart the program & Report on GitHub.");
-                            return;
-                    }
-                }
-                switch (UserOutput)
-                {
-                    case 1:
-                        Way = "Create";
-                        bool repeat;
-                        if (again) repeat = true;
-                        else repeat = false;
-                        do {
-                            CreationResult = CreateCLI.CreatingProcess(CreationResult, again);
-                            repeat = false;
-                            if (CreationResult.Value2 == returnString)
-                            {
-                                Console.WriteLine("Exiting Neural Network Creation...");
-                                Error = true;
-                            }
-                            else {
-                                LoadedNetwork = CreationResult.Value1 ?? throw new Exception("Loaded Network cannot be null");
-                                if (again)
+                        LoadedNetwork = LoadedNetwork?? throw new Exception("Loaded Netwok cannot be null");
+                        switch(UserOutput)
+                        {
+                            case 1:
+                                var runResult = RunCLI.Run_Network(LoadedNetwork);
+                                if (runResult.HasError)
                                 {
-                                    Console.WriteLine("Deleting old Network & Creating new Network...");
-                                    if (SaveCLI.DeleteFile(currentnnName)) Extras.PressKey();
-                                    else //Return Keyword was typed in
-                                    {
-                                        Console.WriteLine("Why do you do that?");
-                                        Console.WriteLine("Do you think I have no Life");
-                                        Console.WriteLine("I have feelings too, you know...");
-                                        Console.WriteLine("Please just type in the same name and everything will be fine...");
-                                        Console.WriteLine();
-                                    }
+                                    if (runResult.ErrorMessage == "User exited run process.") Error = true;
+                                    else Error = true;
                                 }
-                                currentnnName = SaveCLI.SaveNetworkToFile(LoadedNetwork, "new");
-                                again = false;
-                                if (currentnnName.ToLower() == returnString.ToLower()) repeat = true;
-                            }
+                                else {
+                                double[]? output = runResult.Value  ?? throw new Exception("Network Output cannot be null"); //No Error handeling
+                                for(int i = 0; i < output.Length; i++)
+                                {
+                                    Console.WriteLine($"Neuron {i + 1}: {output[i]}");
+                                }
+                                }
+                                break;
+                            case 2:
+                                Console.WriteLine("This feature is in the working process...");
+                                Console.WriteLine("CAREFUL: This feature isn't working yet");
+                                try {
+                                    Error = Learn.UserDialoge(LoadedNetwork);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine();
+                                    Console.WriteLine($"You Exited the Learning Process with the Exception {e.Data}");
+                                }
+                                Console.WriteLine("You Exited the Learning Process Sucessfully");
+                                break;
+                            case 3:
+                                var editResult = EditCLI.Edit_Network(LoadedNetwork, currentnnName);
+                                if (editResult.HasError)                            {
+                                    if (Extras.IsReturn(editResult.ErrorMessage)) {
+                                        Error = true;
+                                        again = true;
+                                    }
+                                    else Error = true;
+                                }
+                                else
+                                {
+                                    Error = true;
+                                }
+                                break;
+                            case 4: 
+                                break;
+                            case 5:
+                                Console.WriteLine("Exiting Program...");
+                                Extras.PressKey();
+                                return;
+                            default:
+                                Console.WriteLine("Please insert one of the shown Options");
+                                Error = true;
+                                break;
                         }
-                        while(repeat);
-                        break;
-                    case 2:
-                        Way = "Load";
-                        var result = LoadCLI.LoadNeuralNetwork();
-                        if (result.HasError)
-                        {
-                            Error = true;
-                            if (result.ErrorMessage != "User exited load process.") Extras.PressKey();
-                        }
-                        else
-                        {
-                            LoadedNetwork = (result.Value ?? throw new Exception("Network and network name cannot be null")).Value1 ?? throw new Exception("Network cannot be null");
-                            currentnnName = result.Value.Value2 ?? throw new Exception("Network name cannot be null");
-                        }
-                        break;
-                    case 3:
-                        Console.WriteLine("Exiting Program...");
-                        return;
-                    default:
-                        if(!Error) {
-                            Console.WriteLine("Please type in one of the shown options");
-                            Extras.PressKey();
-                            Error = true;
-                        }
-                        break;
-                }
-                again = false;
-            } while (Error);
-            if (LoadedNetwork is null) throw new InvalidOperationException("LoadedNetwork must not be null here");
-
-            Extras.PressKey();
-            LoadedNetwork = EditCLI.RandomizeIfNeeded(LoadedNetwork, currentnnName);
-            UserOutput = 0;
-            do {
-                Error = false;
-                Console.WriteLine();
-                Console.WriteLine("What do you want to do?");
-                Console.WriteLine("1. Run the Neural Network");
-                Console.WriteLine("2. Let the Neural Network learn");
-                string UserOutputString = Console.ReadLine() ?? string.Empty;
-                if (!int.TryParse(UserOutputString, out UserOutput))
-                {
-                    if (Extras.IsReturn(UserOutputString)) again = true;
-                    else {
-                        Console.WriteLine("Please type in a valid number");
-                        Error = true;
                     }
                 }
-                else
-                {
-                    LoadedNetwork = LoadedNetwork?? throw new Exception("Loaded Netwok cannot be null");
-                    switch(UserOutput)
-                    {
-                        case 1:
-                            var runResult = RunCLI.Run_Network(LoadedNetwork);
-                            if (runResult.HasError)
-                            {
-                                if (runResult.ErrorMessage == "User exited run process.") Error = true;
-                                else Error = true;
-                            }
-                            else {
-                            double[]? output = runResult.Value  ?? throw new Exception("Network Output cannot be null"); //No Error handeling
-                            for(int i = 0; i < output.Length; i++)
-                            {
-                                Console.WriteLine($"Neuron {i + 1}: {output[i]}");
-                            }
-                            }
-                            break;
-                        case 2:
-                            Console.WriteLine("This feature is in the working process...");
-                            Console.WriteLine("CAREFUL: This feature isn't working yet");
-                            try {
-                                Error = Learn.UserDialoge(LoadedNetwork);
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine();
-                                Console.WriteLine($"You Exited the Learning Process with the Exception {e.Data}");
-                            }
-                            Console.WriteLine("You Exited the Learning Process Sucessfully");
-                            break;
-                        default:
-                            Console.WriteLine("Please insert one of the shown Options");
-                            Error = true;
-                            break;
-                    }
-                }
-            }
-            while (Error);
-        } while (again);
-        Console.WriteLine("Exiting Program...");
-        Extras.PressKey();
+                while (Error);
+            } while (again);
+            Console.WriteLine("Returning to Main Menu...");
+            Console.WriteLine("------------------------------");
+        }
     }
 }
